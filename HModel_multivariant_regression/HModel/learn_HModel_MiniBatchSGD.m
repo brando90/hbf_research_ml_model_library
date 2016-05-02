4e18ea83fd94f93f202244431fd346083bf7bbc7
Brando <brando90@mit.edu>
2016-05-02 10:07:53 -0400

    saving work so far HModel

--- HModel_multivariant_regression/HModel/learn_HModel_MiniBatchSGD.m
+++ HModel_multivariant_regression/HModel/learn_HModel_MiniBatchSGD.m
@@ -1,11 +1,11 @@
-function [ mdl, errors_train, errors_test ] = learn_HReLu_MiniBatchSGD(X_train,Y_train, mdl, iterations, X_test,Y_test, step_size_params, sgd_errors)
+function [ mdl, errors_train, errors_test ] = learn_HModel_MiniBatchSGD(X_train,Y_train, mdl, nb_iterations, X_test,Y_test, step_size_params, sgd_errors)
 fprintf('sgd_errors = %d',sgd_errors);
 [~, K] = size(mdl.t); % (D^(0) x D^(L1))
 [~, D] = size(X_train); % (M x D^(0))
 [~, D_out] = size(Y_train); % (M x D^(2))
 if sgd_errors
-    errors_train = zeros(iterations+1,1);
-    errors_test = zeros(iterations+1,1);
+    errors_train = zeros(nb_iterations+1,1);
+    errors_test = zeros(nb_iterations+1,1);
     errors_train(1) = compute_Hf_sq_error(X_train,Y_train, mdl, mdl.lambda);
     errors_test(1) = compute_Hf_sq_error(X_test,Y_test, mdl, mdl.lambda);
 end
@@ -19,17 +19,17 @@ for i=2:length(errors_test)
     mini_batch_indices = ceil(rand(batchsize,1) * nb_examples);
     Xminibatch =  X_train(mini_batch_indices,:);
     Yminibatch = Y_train(mini_batch_indices,:);
-    %% Forward pass
+    %% Forward Pass (fp)
+%     batchsize = size(Xminibatch,1);
 %     fp(0).A = Xminibatch; % (M x D+1)
 %     fp(1).Z = [ones(batchsize,1), fp(0).A] * mdl.t;  % (M * K) = (M x D+1) x (D+1 * K)
-%     fp(1).A = max(0, fp(1).Z); % (M * K)
-%     fp(2).A = fp.A * mdl.c; % (M x D) = (M * K) * (K * D)
+%     fp(1).A = mdl.Act(0, fp(1).Z); % (M * K)
 %     fp(2).A = [ones(batchsize,1), fp.A] * mdl.c; % (M x D) = (M * K+1) * (K+1 * D)
-    fp = mdl.F(Xminibatch);
+    fp = mdl.F(mdl, Xminibatch);
     %% gradients * backprop
-    backprop(2).delta = (2/batchsize) * (fp(2).A - Yminibatch) .* (fp(2).A > 0); % ( M x D^(L2) )
-    backprop.dJdc = [ones(batchsize,1),fp(1).A]' * backprop(2).delta; % (D^(L1)+1 x D^(L2)) = (M x D ^(L1)+1)' x (M x D^(l))
-    backprop.(1).delta = (fp(1).A > 0) .* backprop(2).delta * c'; % (M x D^(L1)) = (M x D^(L1)) .* (M x D^(L2)) x (D^(L1) x D^(L2))'
+    backprop(2).delta = (2/batchsize) * (fp(2).A - Yminibatch) .* mdl.dAct_ds(fp(2).A); % ( M x D^(L2) )
+    backprop.dJ_dc = [ones(batchsize,1),fp(1).A]' * backprop(2).delta; % (D^(L1)+1 x D^(L2)) = (M x D ^(L1)+1)' x (M x D^(l))
+    backprop.(1).delta = mdl.dAct_ds(fp(1).A) .* backprop(2).delta * c'; % (M x D^(L1)) = (M x D^(L1)) .* (M x D^(L2)) x (D^(L1) x D^(L2))'
     backprop.dJ_dt = fp(0).A' * backprop(1).delta; % (D^(L0)+1 x D^(L1)) = (M x D^(L0))' x (M x D^(L1))
     %% step size
     if step_size_params.AdaGrad
---
+++ multilayer_backprop_HModel_draft.m
@@ -0,0 +1,49 @@
+function [ neural_net, errors_train, errors_test ] = multilayer_backprop_HReLu_draft( X_train,Y_train, neural_net, iterations,batchsize, X_test,Y_test, eta_c, eta_t, sgd_errors )
+fprintf('sgd_errors = %d',sgd_errors);
+[N, ~] = size(X_train);
+[~,D_out] = size(Y_train);
+if sgd_errors
+    errors_train = zeros(iterations+1,1);
+    errors_test = zeros(iterations+1,1);
+    errors_train(1) = compute_Hf_sq_error(X_train,Y_train, mdl, mdl.lambda);
+    errors_test(1) = compute_Hf_sq_error(X_test,Y_test, mdl, mdl.lambda);
+end
+% G_c = ones(K, D_out);
+% G_t = ones(D, K);
+for i=2:length(errors_test)
+    %% get minibatch
+    mini_batch_indices = ceil(rand(batchsize,1) * N); % M
+    Xminibatch =  X_train(mini_batch_indices,:); % ( M x D ) =( M x D^(0) )
+    Yminibatch = Y_train(mini_batch_indices,:); % ( M x D^(L) )
+    A = Xminibatch; % ( M x D+1) = (M x D^(0)+1)
+    %% Forward pass starting from the input
+    for l = 1:nb_layers-1
+        A = max(0, [ones(batchsize,1), A] * neural_net(l).W); % (M x D^(l)) = (M x D^(l-1)+1) x (D^(l-1)+1 x D^(l))
+        prop_computation(l).A = A; % (M x D^(l))
+    end
+    %% Back propagation
+    delta_L = (2 / batchsize)*(Yminibatch - Xminibatch) .* (prop_computation(L).A > 0); % ( M x D^(L) ) = (M x D^(L)) .* (M x D^(L))
+    prop_computation(nb_layers).delta = delta_L; % ( M x D^(L) )
+    step_down_1=-1;
+    for l = nb_layers:step_down_1:2
+        % get gradient matrix dV_dW^(l) for parameters W^(l) at layer l
+        dV_dW_l = [ones(batchsize,1), prop_computation(l-1).A]' * prop_computation(l).delta; % (D^(l-1)+1 x D^(l)) = (M x D ^(l-1)+1)' x (M x D^(l))
+        D_l_1 = size(prop_computation(l-1).A,1); % get value of D^(l-1)
+        dV_dW_l(2:D_l_1+1,:) = dV_dW_l + lambda * neural_net(l).W(2:D_l_1+1,:); % regularize everything except the offset
+        prop_computation(l).dW = dV_dW_l; % (D ^(l-1)+1 x D^(l))
+
+        % compute delta for next iteration of backprop (i.e. previous layer) and threshold at 0 if O is <0 (ReLU gradient update)
+        prop_computation(l-1).delta = (prop_computation(l-1).A > 0) .* prop_computation(l).delta * neural_net(l).W'; % (M x D^(l-1)) = (M x D^(l) x ()
+    end
+    %% step size
+    mod_when = 2000;
+    if mod(i, mod_when) == 0
+        step_size = step_size/1.2;
+    end
+    %% gradient step for all layers
+    for j = 1:nb_layers
+        neural_net(j).W = neural_net(j).W - step_size * prop_computation(j).dW;
+        neural_net(j).b = neural_net(j).b - step_size * prop_computation(j).db;
+    end
+end
+end
