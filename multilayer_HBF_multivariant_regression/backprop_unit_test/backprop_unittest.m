f34ac91b1c47f95cb31fefef36a9a2236ea9e2e1
Brando <brando90@mit.edu>
2016-05-07 15:52:10 -0400

    save work and get a snack

--- multilayer_HBF_multivariant_regression/backprop_unit_test/backprop_unittest.m
+++ multilayer_HBF_multivariant_regression/backprop_unit_test/backprop_unittest.m
@@ -3,6 +3,11 @@ restoredefaultpath;clear;clc;
 folderName = fullfile('..');
 p = genpath(folderName);
 addpath(p);
+%% act funcs
+gauss_func = @(S) exp(S);
+dGauss_ds = @(A) A;
+Identity = @(A) A;
+dIdentity_ds = @(A) ones(size(A));
 %% fake data
 N = 100;
 D = 4;
@@ -15,35 +20,45 @@ mini_batch_indices = ceil(rand(batchsize,1) * N); % M
 Xminibatch =  X_train(mini_batch_indices,:); % ( M x D ) =( M x D^(0) )
 Yminibatch = Y_train(mini_batch_indices,:); % ( M x D^(L) )
 %% Define multilayer HBF net
-gauss_func = @(S) exp(S);
-dGauss_ds = @(A) A;
+%mdl = struct('F',cell(1,L),'Act',cell(1,L),'dAct_ds',cell(1,L),'W',cell(1,L),'beta',cell(1,L));
+F_func_name = 'F_NO_activation_final_layer';
 Act = gauss_func;
 dAct_ds = dGauss_ds;
-h_mdl.Act = Act;
-h_mdl.dAct_ds = dAct_ds;
-h_mdl.F = @F;
-K = 4;
-h_mdl.beta = 0.5;
+for l =1:L-1
+    mdl(l).Act = Act;
+    mdl(l).dAct_ds = dAct_ds;
+    mdl.beta = 0.5;
+end
+switch F_func_name
+    case 'F_NO_activation_final_layer'
+        mdl(L).Act = Identity;
+        mdl(L).dAct_ds = dIdentity_ds;
+    case 'F_activation_final_layer'
+        mdl(L).Act = Act;
+        mdl(L).dAct_ds = dAct_ds;
+end      
+mdl(1).F = @F;
+K = 6;
 t = rand(D,K);
 c = rand(K,D_out);
-h_mdl(1).W = t;
-h_mdl(2).W = c;
+mdl(1).W = t;
+mdl(2).W = c;
 %% Forward pass
-F(h_mdl, Xminibatch);
-[fp] = h_mdl.F(h_mdl, Xminibatch);
+[fp] = mdl(1).F(mdl, Xminibatch);
 %% Calculate derivatives using backprop code
 % compute dJ_dw
+backprop = struct('delta', cell(1,L));
 backprop(L).delta = (2 / batchsize)*( fp(L).A - Yminibatch ) .* mdl(L).dAct_ds( fp(L).A ); % ( M x D^(L) ) = (M x D^(L)) .* (M x D^(L))
 step_down_1=-1;
 for l = L:step_down_1:2
     % get gradient matrix dV_dW^(l) for parameters W^(l) at layer l
-    T_ijm = bsxfun( @times, W, reshape(backprop(l).delta',[1,flip( size(backprop(l).delta) )] ) ); % ( D^(l - 1) x D^(l) x M )
-    backprop(l).dW = 2 * mdl.beta * ( fp(l-1).A'*backprop(l).delta - sum( T_ijm, 3) ); % (D^(l-1) x D^(l)) = (D^(l-1) x D^(l)) .- sum[ (D^(l-1) x D^(l) x M), 3 ] = (D^(l-1) x M) x (M x D^(l)) .- sum[ (D^(l-1) x D^(l) x M), 3 ]
+    T_ijm = bsxfun( @times, mdl(l).W, reshape(backprop(l).delta',[1,flip( size(backprop(l).delta) )] ) ); % ( D^(l - 1) x D^(l) x M )
+    backprop(l).dW = 2 * mdl(1).beta * ( fp(l-1).A'*backprop(l).delta - sum( T_ijm, 3) ); % (D^(l-1) x D^(l)) = (D^(l-1) x D^(l)) .- sum[ (D^(l-1) x D^(l) x M), 3 ] = (D^(l-1) x M) x (M x D^(l)) .- sum[ (D^(l-1) x D^(l) x M), 3 ]
 
     % compute delta for next iteration of backprop (i.e. previous layer) and threshold at 0 if O is <0 (ReLU gradient update)
     delta_sum = sum(backprop(l).delta ,2); % (M x 1) <- sum( (M x L), 2 ) 
     A_delta = bsxfun(@times, fp(l).A, delta_sum); % (M x L) = (M x L) .* (M x 1)
-    backprop(l-1).delta = 2*mdl.beta * hbf_netdAct_ds( fp(l-1).A ).*( backprop(l).delta*mdl(l).W' - A_delta ); % (M x D^(l-1)) = (M x D^(l) x ()
+    backprop(l-1).delta = 2*mdl(1).beta * mdl(l).dAct_ds( fp(l-1).A ).*( backprop(l).delta*mdl(l).W' - A_delta ); % (M x D^(l-1)) = (M x D^(l) x ()
 end
 %% Calcualte numerical derivatives
 %TODO
--- multilayer_HBF_multivariant_regression/model_functions/F.m
+++ multilayer_HBF_multivariant_regression/model_functions/F.m
@@ -2,23 +2,24 @@ function [ fp ] = F( mdl, Xminibatch )
 %% Forward Pass (fp) HBF
 L = size(mdl,2);
 A = Xminibatch; % ( M x D) = (M x D^(0))
+fp = struct('F', cell(1,L));
 for l = 1:L-1
     WW = sum(mdl(l).W .* mdl(l).W, 1); % ( 1 x D^(l) )
     XX = sum(A .* A, 2); % (M x 1)
     moving_offset = bsxfun(@plus, XX, WW); % ( M x D^(l) ) = (M x 1) .+ (1 x D^(l))
-    Z = 2 * mdl.beta * ( A*mdl(l).W - moving_offset); % (M x D^(l)) = (M x D^(l-1)+1) x (D^(l-1)+1 x D^(l))
-    A = mdl.Act(Z); % (M x D^(l))
+    Z = 2 * mdl(1).beta * ( A*mdl(l).W - moving_offset); % (M x D^(l)) = (M x D^(l-1)+1) x (D^(l-1)+1 x D^(l))
+    A = mdl(l).Act(Z); % (M x D^(l))
     fp(l).A = A; % (M x D^(l))
 end
 % activation for final layer
 if mdl(L).Act( ones(1) ) == ones(1) %% if Act func is Identity 
-    A = mdl(L).Act( A * neural_net(L).W ); % (M x D^(l)) = (M x D^(l-1)+1) x (D^(l-1)+1 x D^(l))
+    A = mdl(L).Act( A * mdl(L).W ); % (M x D^(l)) = (M x D^(l-1)+1) x (D^(l-1)+1 x D^(l))
     fp(L).A = A; % (M x D^(l))
 else
     WW = sum(mdl(L).W .* mdl(L).W, 1); % ( 1 x D^(l) )
     XX = sum(A .* A, 2); % (M x 1)
     moving_offset = bsxfun(@plus, XX, WW); % ( M x D^(l) ) = (M x 1) .+ (1 x D^(l))
-    Z = 2 * mdl.beta ( A*mdl(L).W - moving_offset); % (M x D^(l)) = (M x D^(l-1)+1) x (D^(l-1)+1 x D^(l))
+    Z = 2 * mdl(1).beta ( A*mdl(L).W - moving_offset); % (M x D^(l)) = (M x D^(l-1)+1) x (D^(l-1)+1 x D^(l))
     fp(L).A = mdl(L).Act(Z); % (M x D^(l))
 end
 end
