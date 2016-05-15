a924790e813a6853255ab7dadf1d9ccb4576e6d4
Brando <brando90@mit.edu>
2016-05-15 18:50:57 -0400

    pca changed

--- PCA/pca_simple_example.m
+++ PCA/pca_simple_example.m
@@ -1,23 +1,28 @@
-clear;
+clear;clc;
 %% data
 D = 3
 N = 5
 X = rand(D, N);
+X = magic(N); %% <------ uncomment this line for disaster
 %% process data
 x_mean = mean(X, 2); %% computes the mean of the data x_mean = sum(x^(i))
-% x_mean2 = zeros(D,1);
-% for i=1:N
-%     x_mean2 = x_mean2 + X(:,i);
-% end
-% x_mean2 = x_mean2/N
 X_centered = X - repmat(x_mean, [1,N]);
 %% PCA
 [coeff, score, latent, ~, ~, mu] = pca(X'); % coeff =  U
 [U, S, V] = svd(X_centered); % coeff = U
+%%
+U
+coeff
+V
 %% Reconstruct data
+% if U = coeff then the following should be an identity I (since U is orthonormal)
+UU_T =  U * U'
+coeff_coeff_T = coeff * coeff'
+% if U = coeff then they should be able to perfectly reconstruct the data
 X_tilde_U = U * U'*X
 X_tilde_coeff = coeff*coeff'*X
-X_tilde_score = ( score * coeff' + + repmat(mu,[N,1]) )';
+
+%X_tilde_score = ( score * coeff' + repmat(mu,[N,1]) )'
 
 % latent
 % latent.^(2)
