425d5ad42c3904b88fe07760fbc432b563473ecb
Brando <brando90@mit.edu>
2016-05-25 20:14:02 -0400

    step size code

--- multilayer_HBF_multivariant_regression/multilayer_learn_HBF_MiniBatchSGD.m
+++ multilayer_HBF_multivariant_regression/multilayer_learn_HBF_MiniBatchSGD.m
@@ -9,17 +9,10 @@ if sgd_errors
     errors_train(1) = compute_Hf_sq_error(X_train,Y_train, mdl);
     errors_test(1) = compute_Hf_sq_error(X_test,Y_test, mdl);
 end
-if step_size_params.AdaGrad
-    G_c = ones(K, D_out);
-    G_t = ones(D, K);
-elseif step_size_params.Decaying
-    step_size = step_size_params.step_size; %TODO
-end
+fprintf ('Iter %d. Training zero-one error: %f; Testing zero-one error: %f; eta_W =%f , eta_Std =%f \n', 0, errors_train(1), errors_test(1), step_size_params.W(1).eta, step_size_params.Std(1).eta)
+%% SGD/Mini Batch
 fp = struct('A', cell(1,L), 'Z', cell(1,L), 'Delta_tilde', cell(1,L));
 backprop = struct('delta', cell(1,L), 'dW', cell(1,L), 'dStd', cell(1,L));
-
-fprintf ('Iter %d. Training zero-one error: %f; Testing zero-one error: %f; step size =%f \n', 0, errors_train(1), errors_test(1), step_size)
-
 for i=2:length(errors_test)
     %% get minibatch
     mini_batch_indices = ceil(rand(batchsize,1) * N); % M
@@ -80,22 +73,34 @@ for i=2:length(errors_test)
     backprop(l).dW = 2 * mdl(l).beta * ( Xminibatch'*backprop(l).delta - sum( T_ijm, 3) ); % (D^(l-1) x D^(l)) = (D^(l-1) x D^(l)) .- sum[ (D^(l-1) x D^(l) x M), 3 ] = (D^(l-1) x M) x (M x D^(l)) .- sum[ (D^(l-1) x D^(l) x M), 3 ]
     backprop(l).dStd = -8^0.5 * mdl(l).beta^1.5 * sum( sum(backprop(l).delta .* fp(l).Delta_tilde) ); % (1 x 1)  = sum(sum( (N x D^(l)) .x (N x D^(l)) ))
     %% step size
-    mod_when = step_size_params.mod_when;
     if step_size_params.AdaGrad
-        G_c = G_c + dJ_dc.^2;
-        mu_c = step_size_params.eta_c ./ ( (G_c).^0.5 );
-        G_t = G_t + dJ_dt.^2;
-        mu_t = step_size_params.eta_t ./ ( (G_t).^0.5 );
-    elseif step_size_params.Decaying
-        if mod(i, mod_when) == 0
-            step_size = step_size/step_size_params.decay_rate;
+        for l=1:L
+            G_w = G_w + backprop(l).dW.^2;
+            step_size_params.eta(l).W = step_size_params.eta(l).W ./ ( (G_w).^0.5 );
+            G_std = G_std + backprop(l).dStd.^2;
+            step_size_params.eta(l).Std = step_size_params.eta(l).Std ./ ( (G_std).^0.5 );
+        end
+    elseif step_size_params.Momentum
+        % do momentum
+        error('TODO');
+    end
+    % decay constant infront of step-size algorithm
+    if mod(i, step_size_params.Std(l).decay_frequency) == 0
+        for l=1:L
+            step_size_params.eta(l).W = step_size_params.W(l).eta/step_size_params.W(l).decay_rate;
         end
     end
-    
+    if mod(i, step_size_params.Std(l).decay_frequency) == 0
+        for l=1:L
+            step_size_params.eta(l).Std = step_size_params.Std(l).eta/step_size_params.Std(l).decay_rate;
+        end
+    end
     %% gradient step for all layers
     for l = 1:L
-        mdl(l).W = mdl(l).W - step_size * backprop(l).dW;
-        std_new = ( 1/realsqrt(2 * mdl(l).beta) ) - step_size * backprop(l).dStd;
+        % W = W - eta dJdW
+        mdl(l).W = mdl(l).W - step_size_params.W(l).eta * backprop(l).dW;
+        % std = std - eta dJdstd
+        std_new = ( 1/realsqrt(2 * mdl(l).beta) ) - step_size_params.Std(l).eta * backprop(l).dStd;
         mdl(l).beta = 1/(2*std_new^2);
     end
     %% print errors
